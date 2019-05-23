d3ecb822b187fb506433c180a88ef44442c8edb3
Mahesha N C <Mahesha.NC@Ltts.com>
2019-05-23 21:00:38 +0530

    react dnd library added

--- src/components/drag/DragDrop.component.js
+++ src/components/drag/DragDrop.component.js
@@ -1,121 +1,102 @@
-import React, { Component } from 'react';
-import BackendComponent from './Backend.component';
-import FrontendComponent from './Frontend.component';
+import React from 'react';
+import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
+import initialData from './initial-data'
+import Column from './column'
+
+const Container = styled.div`
+  display:flex;
+`
 
 class DragDropComponent extends React.Component {
+    state = initialData
 
-    state = {
-        frontend: [
-            { 
-                name:"Learn Angular",
-                category:"frontend", 
-                bgcolor: "yellow"
-            },  
-            {
-                name:"React", 
-                category:"frontend", 
-                bgcolor:"pink"
-            },  
-            {   name:"Vue", 
-                category:"frontend", 
-                bgcolor:"skyblue"
-            }          
-        ],  
-        backend: [
-            { 
-                name:"Java",
-                category:"backend", 
-                bgcolor: "green"
-            },  
-            {
-                name:"Node", 
-                category:"backend", 
-                bgcolor:"black"
-            },  
-            {   name:"PHP", 
-                category:"backend", 
-                bgcolor:"blue"
-            }  
-        ]
+    onDragEnd = result => {
+        const { destination, source, draggableId } = result
 
-    }
+        if (!destination) {
+        return
+        }
 
-    onDragStart = (ev, id) => {
-        console.log("Drag start", id);
-        ev.dataTransfer.setData("id", id);
-    }
+        if (
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index
+        ) {
+        return
+        }
 
-    onDragOver = (ev) => {
-        ev.preventDefault();
-    }
+        const start = this.state.columns[source.droppableId]
+        const finish = this.state.columns[destination.droppableId]
 
-    onDrop = (ev, cat) => {
-        let id = ev.dataTransfer.getData('id');
+        if (start === finish) {
+            const newTaskIds = Array.from(start.taskIds)
+            newTaskIds.splice(source.index, 1)
+            newTaskIds.splice(destination.index, 0, draggableId)
 
-        let tasks = this.state.data.filter((task) => {
-            if(task.name == id) {
-                task.category = cat; 
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds
             }
-            return task;
-        })
 
-        this.setState({...this.state, tasks});
+            const newState = {
+                ...this.state,
+                columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn
+                }
+            }
+
+            this.setState(newState)
+            return
+        }
+
+        // Moving from one list to another
+        const startTaskIds = Array.from(start.taskIds)
+        startTaskIds.splice(source.index, 1)
+        const newStart = {
+        ...start,
+        taskIds: startTaskIds
+        }
+
+        const finishTaskIds = Array.from(finish.taskIds)
+        finishTaskIds.splice(destination.index, 0, draggableId)
+        const newFinish = {
+        ...finish,
+        taskIds: finishTaskIds
+        }
+
+        const newState = {
+        ...this.state,
+        columns: {
+            ...this.state.columns,
+            [newStart.id]: newStart,
+            [newFinish.id]: newFinish
+        }
+        }
+        this.setState(newState)
     }
 
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Container>
+          {this.state.columnOrder.map(columnId => {
+            const column = this.state.columns[columnId]
+            const tasks = column.taskIds.map(
+              taskId => this.state.tasks[taskId]
+            )
 
-    removetechnology = technology => {
-        this.setState({
-            backend: this.state.backend.filter((techName, i) => { 
-                return i !== technology;
-            })
-        });
-    }
-    
-    render() {
-        
-        var tasks = {
-            wip: [], 
-            complete: []
-        };
-
-        // this.state.data.forEach((t) => {
-        //     tasks[t.category].push(
-        //         <div key={t.name} onDragStart={ (e)=> this.onDragStart(e, t.name)} draggable className="draggable" style={{backgroundColor: t.bgcolor}}>
-        //                 {t.name}
-        //         </div>
-        //     )
-        // })
-
-        return(
-            // <div className="container-drag">
-            //     <h2 className="header"> DRAG and DROP </h2>
-            //     <div className="wip" onDragOver={(e) => this.onDragOver(e)} onDrop={ (e) => this.onDrop(e, "wip")}>
-            //         <span className="task-header"> WIP  </span>
-            //             {tasks.wip}
-            //     </div>
-            //     <div className="droppable" onDragOver={(e) => this.onDragOver(e)} 
-            //         onDrop={(e) => this.onDrop(e, "complete")}>
-            //        <span className="task-header"> Completed </span>
-            //        {tasks.complete} 
-            //     </div>
-            // </div>
-
-            <div className="container">
-                <br/>
-                <div className="row">
-                    <div className="col-md-4">
-                        <FrontendComponent frontendData={this.state.frontend}  backendData={this.state.backend} />
-                    </div>
-                    <div className="col-md-4">
-                    </div>
-                    <div className="col-md-4">
-                        <BackendComponent backendData={this.state.backend}/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+            return (
+              <Column key={column.id} column={column} tasks={tasks} />
+            )
+          })}
+        </Container>
+      </DragDropContext>
+    )
+  }
 }
 
 export default DragDropComponent;
+
